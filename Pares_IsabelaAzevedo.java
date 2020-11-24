d3c02703858721c8bbf04f7ff7ed9eb95f54e553
isaazev17 <73084383+isaazev17@users.noreply.github.com>
2020-11-23 22:58:59 -0300

    Add files via upload

---
+++ Pares_IsabelaAzevedo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pares_isabelaazevedo;
+import java.util.Scanner;
+
+/** 
+ *
+ * @author Isabela
+ */
+public class Pares_IsabelaAzevedo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Digite um numero:" );
+        int numero = 1000;
+        while (numero<1500)
+        {  numero+=2; 
+        System.out.println(numero);
+        } 
+    }
+    
+}
